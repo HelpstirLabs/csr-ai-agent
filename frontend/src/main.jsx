fc95fd9a60a4c1dbb991fab5c4955e891729b023
manjeshcoder <manja29june@gmail.com>
2026-09-23 22:40:49 +0530

    csr to fundstir 2nd commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/csr">
+  <BrowserRouter basename="/fundstir">
     <AuthProvider>
       <App />
     </AuthProvider>
